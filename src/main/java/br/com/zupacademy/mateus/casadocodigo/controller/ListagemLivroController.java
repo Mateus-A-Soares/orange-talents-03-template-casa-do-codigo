e301712f36fe23be80799e6c33e0a74583c4995d
Mateus-A-Soares <soares.almeida.mateus@gmail.com>
2021-04-05 17:00:53 -0300

    End point detalhando livro implementado

--- src/main/java/br/com/zupacademy/mateus/casadocodigo/controller/ListagemLivroController.java
+++ src/main/java/br/com/zupacademy/mateus/casadocodigo/controller/ListagemLivroController.java
@@ -1,12 +1,17 @@
 package br.com.zupacademy.mateus.casadocodigo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.zupacademy.mateus.casadocodigo.model.Livro;
+import br.com.zupacademy.mateus.casadocodigo.model.response.LivroResponse;
 import br.com.zupacademy.mateus.casadocodigo.model.response.SimpleLivroResponse;
 import br.com.zupacademy.mateus.casadocodigo.repository.LivroRepository;
 
@@ -33,5 +38,20 @@ public class ListagemLivroController {
 	public ResponseEntity<List<SimpleLivroResponse>> listar() {
 		List<SimpleLivroResponse> responseList = livroRepository.selectIdTituloFromLivro();
 		return ResponseEntity.ok().body(responseList);
+	}
+	
+	/**
+	 * End-point de URL /livros/{id} que realiza a pesquisa pelo id de um livro, retornando o registro se for encontrado.
+	 * 
+	 * @param id id passado no path.
+	 * @return ResponseEntity representando o status HTTP 200, 404 ou 500.
+	 */
+	@GetMapping("/{id}")
+	public ResponseEntity<LivroResponse> detalhar(@PathVariable Long id) {
+		Optional<Livro> livroOptional = livroRepository.findById(id);
+		if(livroOptional.isEmpty())
+			return ResponseEntity.notFound().build();
+		LivroResponse response = new LivroResponse(livroOptional.get());
+		return ResponseEntity.ok().body(response);
 	}
 }
